3bd422ffb3be1be0dd005f39d02b13da35c8e886
dmytro.komisaryk <dmytrokomisaryk@gmail.com>
2015-08-19 00:12:50 +0300

    add ordering but votes count

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,6 +2,8 @@ class Option < ActiveRecord::Base
 
   belongs_to :poll
 
+  scope :order_votes, -> { order('votes DESC') }
+
   validates :title, presence: true,
             length: { minimum: 3 }
 
